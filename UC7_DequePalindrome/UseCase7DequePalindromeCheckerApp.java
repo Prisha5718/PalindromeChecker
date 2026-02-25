1f867a4c0faac44840985b1c1d4a9b25ebac349b
Prisha5718 <prishak2110@gmail.com>
2026-02-25 11:25:35 +0530

    Initial clean commit - Palindrome Checker Project

---
+++ UC7_DequePalindrome/UseCase7DequePalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class UseCase7DequePalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "radar";
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string is a Palindrome.");
+        } else {
+            System.out.println("The string is NOT a Palindrome.");
+        }
+    }
+}
